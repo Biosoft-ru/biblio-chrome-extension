7d9951dd7000c52a460190d4a2f6b2b09b3aee87
uuinnk <uuinnk@developmentontheedge.com>
2018-04-20 19:32:15 +0700

    add importance, keyWords

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
 //debugger;
 
-let projectInfo = '<div class="project-info">\n' +
-  '    <h3>${name}</h3>\n' +
-  '    <p><b>Comment:</b> ${comment}</p>\n' +
-  '    <div>\n' +
-  '        <a href="http://biblio3.biouml.org/#!form/publications/Compact%20view/Edit/_cat_=2/selectedRows=2">Edit</a>\n' +
-  '    </div>\n' +
+let projectTemplate = '<div class="project-info">\n' +
+  '    <h3>${name}</h3> <span><b> Imp.: </b>${importance}</span> <a href="http://biblio3.biouml.org/#!form/publications/Compact%20view/Edit/_cat_=2/selectedRows=2">Edit</a>\n' +
+  '    <p><b>keyWords: </b>${keyWords}</p>\n' +
+  '    <p><b>Comment: </b>${comment}</p>\n' +
   '</div>';
 
 load();
@@ -14,17 +12,26 @@ function load() {
 
   const pmids = {
     29669169: {
-      "Demo": {
-        id: 2,
-        comment: "test comment"
-      },
-      "GTRD": {
-        id: 4
+      id: 2,
+      projects: {
+        "Demo": {
+          id: 2,
+          importance: 3,
+          comment: "test comment"
+        },
+        "GTRD": {
+          id: 4,
+          importance: 3,
+        }
       }
     },
     29668588: {
-      "Demo": {
-        id: 2
+      id: 3,
+      projects: {
+        "Demo": {
+          id: 2,
+          importance: 3,
+        }
       }
     }
   };
@@ -38,11 +45,24 @@ function load() {
     let text = '';
     if (articleID in pmids)
     {
-      $.each( pmids[articleID], function( name, attr ) {
-        text += projectInfo
+      $.each(pmids[articleID].projects, function( name, attr ) {
+        let project = projectTemplate
           .replace('${name}', name)
           .replace('${id}', attr.id)
-          .replace('${comment}', attr.comment);
+          .replace('${importance}', attr.importance);
+
+        if(attr.comment !== undefined){
+          project = project.replace('${comment}', attr.comment)
+        }else{
+          project = project.replace('<p><b>Comment: </b>${comment}</p>', '')
+        }
+        if(attr.keyWords !== undefined){
+          project = project.replace('${keyWords}', attr.keyWords)
+        }else{
+          project = project.replace('<p><b>keyWords: </b>${keyWords}</p>', '')
+        }
+
+        text += project;
       });
     }
     else
