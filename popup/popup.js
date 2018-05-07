0330cffcfef6c290fc462d4a2b17e64a764cf3f9
uuinnk <uuinnk@developmentontheedge.com>
2018-05-07 11:23:24 +0700

    comment unused

--- popup/popup.js
+++ popup/popup.js
@@ -75,8 +75,8 @@ function checkState() {
 
 checkState();
 
-function parseJwt(token) {
-  const base64Url = token.split('.')[1];
-  const base64 = base64Url.replace('-', '+').replace('_', '/');
-  return JSON.parse(window.atob(base64));
-}
+// function parseJwt(token) {
+//   const base64Url = token.split('.')[1];
+//   const base64 = base64Url.replace('-', '+').replace('_', '/');
+//   return JSON.parse(window.atob(base64));
+// }
